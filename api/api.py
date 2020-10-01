a21a80693f84831f6dae5b2e69421106719930ad
eddyangang <eddyyang@berkeley.edu>
2020-10-01 16:17:55 -0700

    added static pth

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request
 from scrape import scrapeAndSummarize
 import json
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
